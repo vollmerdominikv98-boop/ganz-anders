3f0a33b5130c473c439e17fee11dd55fa2474f35
vollmerdominikv98-boop <vollmerdominik.v98@gmail.com>
2026-08-16 21:13:15 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-export const DEFAULT_SUPABASE_URL = "https://zmyxigtqkqkwwxssritg.supabase.co";
-export const DEFAULT_SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtcXdpZ3RxaXFsbnZhc3V6cmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5Mjg5MTgsImV4cCI6MjA4NzUwNDkxOH0.1_HqL3uYf_Z1sB3M6nE0Q0v2s3R1_4v5X6Z7s8Q9a0";
+export const DEFAULT_SUPABASE_URL = "";
+export const DEFAULT_SUPABASE_KEY = "";
 export const ADMIN_PASSWORD = "cnc2026";
 
 export const defaultDb = {
@@ -22,94 +22,69 @@ export const defaultDb = {
         "gering": { "name": "Gering (Leichte C-Frame / Lange Auskragung)", "factor": 0.85 }
     },
     profiles: {
-        "schruppen": { "name": "Schruppen (Roughing)", "ap_factor": 1.0, "ae_factor": 0.7, "fz_ratio": 1.0, "rct_active": true, "description": "Maximale Zerspanungsleistung, RCT an" },
-        "schlichten": { "name": "Schlichten (Finishing)", "ap_factor": 0.1, "ae_factor": 0.1, "fz_ratio": 0.6, "rct_active": false, "description": "Gute Oberflächengüte, kleines fz, RCT aus" },
-        "schlichten_gehaertet": { "name": "Feinschlichten Gehärtet (50-65 HRC)", "ap_factor": 0.03, "ae_factor": 0.02, "fz_ratio": 0.35, "rct_active": false, "description": "Feinstschlichten, Schnittdruckkontrolle, RCT aus" },
-        "hsc_trochoidal": { "name": "HSC / Trochoidal (HPC)", "ap_factor": 1.5, "ae_factor": 0.08, "fz_ratio": 1.0, "rct_active": true, "description": "Max ap, kleines ae, RCT aktiv" },
-        "planfraesen": { "name": "Planfräsen (Face Milling)", "ap_factor": 0.5, "ae_factor": 0.8, "fz_ratio": 1.0, "rct_active": false, "description": "Konstanter Eingriff" }
+        "schruppen": { "name": "Schruppen (Roughing)", "ap_factor": 1.0, "ae_factor": 0.7, "fz_ratio": 1.0, "rct_active": true },
+        "schlichten": { "name": "Schlichten (Finishing)", "ap_factor": 0.1, "ae_factor": 0.1, "fz_ratio": 0.6, "rct_active": false },
+        "schlichten_gehaertet": { "name": "Feinschlichten Gehärtet (50-65 HRC)", "ap_factor": 0.05, "ae_factor": 0.05, "fz_ratio": 0.35, "rct_active": false, "max_ap_limit": 0.25, "max_ae_limit": 0.20 },
+        "hsc_trochoidal": { "name": "HSC / Trochoidal (HPC)", "ap_factor": 1.5, "ae_factor": 0.08, "fz_ratio": 1.0, "rct_active": true },
+        "planfraesen": { "name": "Planfräsen (Face Milling)", "ap_factor": 0.5, "ae_factor": 0.8, "fz_ratio": 1.0, "rct_active": false }
     },
     tools: {
-        // --- HARTBEARBEITUNGSSPEZIALISTEN (YG-1 X5070: NUR FÜR GEHÄRTETEN STAHL) ---
         "yg1_x5070_d10_r10": { 
-            "name": "Torusfräser D10 R1.0 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 1.0, "material_schneidstoff": "VHM", "diameter": 10, "z": 4, "max_overhang": 35, "fz_nom": 0.045, 
+            "name": "Torusfräser D10 R1.0 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 1.0, "material_schneidstoff": "VHM", "diameter": 10, "z": 4, "max_overhang": 35, "flute_len": 12, "fz_nom": 0.045, 
             "vc_per_material": { "gehaertet_55hrc": 110, "gehaertet_60hrc": 85, "gehaertet_65hrc": 60 }, 
             "suitable_materials": ["gehaertet_55hrc", "gehaertet_60hrc", "gehaertet_65hrc"], 
             "suitable_profiles": ["schlichten_gehaertet", "hsc_trochoidal"] 
         },
         "yg1_x5070_d8_r05": { 
-            "name": "Torusfräser D8 R0.5 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 0.5, "material_schneidstoff": "VHM", "diameter": 8, "z": 4, "max_overhang": 28, "fz_nom": 0.038, 
+            "name": "Torusfräser D8 R0.5 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 0.5, "material_schneidstoff": "VHM", "diameter": 8, "z": 4, "max_overhang": 28, "flute_len": 10, "fz_nom": 0.038, 
             "vc_per_material": { "gehaertet_55hrc": 110, "gehaertet_60hrc": 85, "gehaertet_65hrc": 60 }, 
             "suitable_materials": ["gehaertet_55hrc", "gehaertet_60hrc", "gehaertet_65hrc"], 
             "suitable_profiles": ["schlichten_gehaertet", "hsc_trochoidal"] 
         },
         "yg1_x5070_d6_r05": { 
-            "name": "Torusfräser D6 R0.5 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 0.5, "material_schneidstoff": "VHM", "diameter": 6, "z": 4, "max_overhang": 22, "fz_nom": 0.030, 
+            "name": "Torusfräser D6 R0.5 Z4", "brand": "YG-1", "line": "X5070", "type": "Torusfräser", "geo_type": "torus", "radius": 0.5, "material_schneidstoff": "VHM", "diameter": 6, "z": 4, "max_overhang": 22, "flute_len": 8, "fz_nom": 0.030, 
             "vc_per_material": { "gehaertet_55hrc": 105, "gehaertet_60hrc": 80, "gehaertet_65hrc": 55 }, 
             "suitable_materials": ["gehaertet_55hrc", "gehaertet_60hrc", "gehaertet_65hrc"], 
             "suitable_profiles": ["schlichten_gehaertet", "hsc_trochoidal"] 
         },
         "yg1_x5070_d6_kugel": { 
-            "name": "Kugelfräser D6 R3.0 Z2", "brand": "YG-1", "line": "X5070", "type": "Kugelfräser", "geo_type": "ball", "radius": 3.0, "material_schneidstoff": "VHM", "diameter": 6, "z": 2, "max_overhang": 25, "fz_nom": 0.035, 
+            "name": "Kugelfräser D6 R3.0 Z2", "brand": "YG-1", "line": "X5070", "type": "Kugelfräser", "geo_type": "ball", "radius": 3.0, "material_schneidstoff": "VHM", "diameter": 6, "z": 2, "max_overhang": 25, "flute_len": 6, "fz_nom": 0.035, 
             "vc_per_material": { "gehaertet_55hrc": 120, "gehaertet_60hrc": 95, "gehaertet_65hrc": 70 }, 
             "suitable_materials": ["gehaertet_55hrc", "gehaertet_60hrc", "gehaertet_65hrc"], 
             "suitable_profiles": ["schlichten_gehaertet"] 
         },
-
-        // --- STAHL- & HPC-ALLROUNDER (GARANT MASTERSTEEL: FÜR WEICHEN & ZÄHEN STAHL / SCHRUPPEN & SCHLICHTEN) ---
         "garant_master_d16": { 
-            "name": "Schaftfräser D16 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 16, "z": 4, "max_overhang": 50, "fz_nom": 0.100, 
+            "name": "Schaftfräser D16 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 16, "z": 4, "max_overhang": 50, "flute_len": 32, "fz_nom": 0.100, 
             "vc_per_material": { "stahl_c45": 200, "stahl_1200": 160, "rostfrei": 110 }, 
             "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal", "planfraesen"] 
         },
         "garant_master_d12": { 
-            "name": "Schaftfräser D12 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 12, "z": 4, "max_overhang": 40, "fz_nom": 0.080, 
+            "name": "Schaftfräser D12 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 12, "z": 4, "max_overhang": 40, "flute_len": 26, "fz_nom": 0.080, 
             "vc_per_material": { "stahl_c45": 200, "stahl_1200": 160, "rostfrei": 110 }, 
             "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal", "planfraesen"] 
         },
         "garant_master_d10": { 
-            "name": "Schaftfräser D10 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 10, "z": 4, "max_overhang": 32, "fz_nom": 0.068, 
+            "name": "Schaftfräser D10 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 10, "z": 4, "max_overhang": 32, "flute_len": 22, "fz_nom": 0.068, 
             "vc_per_material": { "stahl_c45": 200, "stahl_1200": 160, "rostfrei": 110 }, 
             "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal", "planfraesen"] 
         },
         "garant_master_d8": { 
-            "name": "Schaftfräser D8 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 8, "z": 4, "max_overhang": 26, "fz_nom": 0.055, 
+            "name": "Schaftfräser D8 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 8, "z": 4, "max_overhang": 26, "flute_len": 19, "fz_nom": 0.055, 
             "vc_per_material": { "stahl_c45": 190, "stahl_1200": 150, "rostfrei": 100 }, 
             "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal"] 
         },
-        "garant_master_d6": { 
-            "name": "Schaftfräser D6 Z4 HPC", "brand": "Garant", "line": "MasterSteel", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 6, "z": 4, "max_overhang": 20, "fz_nom": 0.042, 
-            "vc_per_material": { "stahl_c45": 180, "stahl_1200": 140, "rostfrei": 90 }, 
-            "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"], 
-            "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal"] 
-        },
-
-        // --- UNIVERSAL-WERKZEUGE (PRECITOOL PREMUS) ---
-        "precitool_premus_d10": {
-            "name": "PREMUS GP Universal D10 Z4", "brand": "PRECITOOL", "line": "PREMUS", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 10, "z": 4, "max_overhang": 32, "fz_nom": 0.060,
-            "vc_per_material": { "stahl_c45": 180, "stahl_1200": 140, "rostfrei": 90 },
-            "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"],
-            "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal"]
-        },
-        "precitool_premus_d8": {
-            "name": "PREMUS GP Universal D8 Z4", "brand": "PRECITOOL", "line": "PREMUS", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 8, "z": 4, "max_overhang": 26, "fz_nom": 0.048,
-            "vc_per_material": { "stahl_c45": 180, "stahl_1200": 140, "rostfrei": 90 },
-            "suitable_materials": ["stahl_c45", "stahl_1200", "rostfrei"],
-            "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal"]
-        },
-
-        // --- ALU-SPEZIALISTEN (HOFFMANN ALULINE: NUR FÜR ALUMINIUM) ---
         "hoffmann_alu_d12": { 
-            "name": "Alu-Fräser D12 Z3", "brand": "Hoffmann", "line": "AluLine", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 12, "z": 3, "max_overhang": 45, "fz_nom": 0.120, 
+            "name": "Alu-Fräser D12 Z3", "brand": "Hoffmann", "line": "AluLine", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 12, "z": 3, "max_overhang": 45, "flute_len": 26, "fz_nom": 0.120, 
             "vc_per_material": { "alu_wrought": 650 }, 
             "suitable_materials": ["alu_wrought"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal", "planfraesen"] 
         },
         "hoffmann_alu_d8": { 
-            "name": "Alu-Fräser D8 Z3", "brand": "Hoffmann", "line": "AluLine", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 8, "z": 3, "max_overhang": 30, "fz_nom": 0.085, 
+            "name": "Alu-Fräser D8 Z3", "brand": "Hoffmann", "line": "AluLine", "type": "Schaftfräser", "geo_type": "shaft", "radius": 0, "material_schneidstoff": "VHM", "diameter": 8, "z": 3, "max_overhang": 30, "flute_len": 19, "fz_nom": 0.085, 
             "vc_per_material": { "alu_wrought": 600 }, 
             "suitable_materials": ["alu_wrought"], 
             "suitable_profiles": ["schruppen", "schlichten", "hsc_trochoidal"] 
@@ -119,3 +94,11 @@ export const defaultDb = {
     history: [],
     swarm_data: []
 };
+
+// Security Helper (XSS Protection)
+export function escapeHTML(str) {
+    if (typeof str !== 'string') return '';
+    return str.replace(/[&<>'"]/g, tag => ({
+        '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;'
+    }[tag] || tag));
+}
